77cbc109dace8dbdcfe23bf7d2b3f1cb05586fc6
mehrdokht <40535304+mehrdokht@users.noreply.github.com>
2021-03-28 13:52:34 +0430

    Add files via upload

---
+++ budget/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Budget(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Budget, self).save()
+
+class MonthlyBudget(models.Model):
+    budget = models.ForeignKey('Budget')
+    month = models.DateField()
+    planned = models.DecimalField(max_digits=65, decimal_places=2)
+    actual = models.DecimalField(max_digits=65, decimal_places=2, blank=True, null=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.slug
+
+    def save(self, *args, **kwargs):
+        forslug = "{0.budget}-{0.month}".format(self)
+        self.slug = slugify(forslug)
+        super(MonthlyBudget, self).save()
